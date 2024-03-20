bf46373c8f65d09015fef3a887b7b8438dd2d7ab
Farhan Ramadhan <farhanramadhan4944@gmail.com>
2024-03-20 20:36:59 +0700

    nineth commit:mobile sidebar md visible

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "./mode-toogle";
 
 export const MobileHeader = () => {
   return (
-    <nav className="md:hidden px-6 h-[50px] flex justify-between items-center bg-indigo-700 dark:bg-indigo-950 bg-opacity-75 border-b fixed top-0 w-full z-50">
+    <nav className="lg:hidden px-6 h-[50px] md:h-20 flex justify-between items-center bg-indigo-700 dark:bg-indigo-950 bg-opacity-75 border-b fixed top-0 w-full z-50">
       <MobileSidebar />
       <ModeToggle />
     </nav>
